af6ac7cbf9e8c81b995cf304ce27efc7e41f9ab7
Marc Hirdes <hirdes@clickstorm.de>
2013-11-25 12:05:40 +0100

    Add new Files for Categories

---
+++ Classes/Domain/Model/Category.php
@@ -0,0 +1,134 @@
+<?php
+namespace TYPO3\GoMapsExt\Domain\Model;
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2012 Marc Hirdes <Marc_Hirdes@gmx.de>, clickstorm GmbH
+ *  
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ *
+ *
+ * @package go_maps_ext
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
+ *
+ */
+class Category extends \TYPO3\CMS\Extbase\DomainObject\AbstractEntity {
+
+	/**
+	 * Name
+	 *
+	 * @var \string
+	 * @validate NotEmpty
+	 */
+	protected $name;
+	
+	/**
+	 * addresses
+	 *
+	 * @var \TYPO3\CMS\Extbase\Persistence\ObjectStorage<\TYPO3\GoMapsExt\Domain\Model\Address>
+	 * @lazy
+	 */
+	protected $addresses;
+	
+	/**
+	 * __construct
+	 *
+	 * @return void
+	 */
+	public function __construct() {
+		//Do not remove the next line: It would break the functionality
+		$this->initStorageObjects();
+	}
+
+	/**
+	 * Initializes all \TYPO3\CMS\Extbase\Persistence\ObjectStorage properties.
+	 *
+	 * @return void
+	 */
+	protected function initStorageObjects() {
+		/**
+		 * Do not modify this method!
+		 * It will be rewritten on each save in the extension builder
+		 * You may modify the constructor of this class instead
+		 */
+		$this->addresses = new \TYPO3\CMS\Extbase\Persistence\ObjectStorage();
+	}
+
+	/**
+	 * Returns the name
+	 *
+	 * @return \string $name
+	 */
+	public function getName() {
+		return $this->name;
+	}
+
+	/**
+	 * Sets the name
+	 *
+	 * @param \string $name
+	 * @return void
+	 */
+	public function setTitle($name) {
+		$this->name = $name;
+	}
+	
+	/**
+	 * Adds a Address
+	 *
+	 * @param \TYPO3\GoMapsExt\Domain\Model\Address $address
+	 * @return void
+	 */
+	public function addAddress(\TYPO3\GoMapsExt\Domain\Model\Address $address) {
+		$this->addresses->attach($address);
+	}
+
+	/**
+	 * Removes a Address
+	 *
+	 * @param \TYPO3\GoMapsExt\Domain\Model\Address $addressToRemove The Address to be removed
+	 * @return void
+	 */
+	public function removeAddress(\TYPO3\GoMapsExt\Domain\Model\Address $addressToRemove) {
+		$this->addresses->detach($addressToRemove);
+	}
+
+	/**
+	 * Returns the addresses
+	 *
+	 * @return \TYPO3\CMS\Extbase\Persistence\ObjectStorage<\TYPO3\GoMapsExt\Domain\Model\Address> $addresses
+	 */
+	public function getAddresses() {
+		return $this->addresses;
+	}
+
+	/**
+	 * Sets the addresses
+	 *
+	 * @param \TYPO3\CMS\Extbase\Persistence\ObjectStorage<\TYPO3\GoMapsExt\Domain\Model\Address> $addresses
+	 * @return void
+	 */
+	public function setAddresses(\TYPO3\CMS\Extbase\Persistence\ObjectStorage $addresses) {
+		$this->addresses = $addresses;
+	}
+}
+?>
---
+++ Configuration/TCA/Category.php
@@ -0,0 +1,146 @@
+<?php
+if (!defined ('TYPO3_MODE')) {
+	die ('Access denied.');
+}
+
+$TCA['tx_gomapsext_domain_model_category'] = array(
+	'ctrl' => $TCA['tx_gomapsext_domain_model_category']['ctrl'],
+	'interface' => array(
+		'showRecordFieldList' => 'sys_language_uid, l10n_parent, l10n_diffsource, hidden, name, addresses',
+	),
+	'types' => array (
+		'0' => array('showitem' => 'name,
+									--div--;LLL:EXT:go_maps_ext/Resources/Private/Language/locallang_db.xml:tx_gomapsext_domain_model_category.others,sys_language_uid;;;;1-1-1, l10n_parent, l10n_diffsource, hidden,starttime, endtime')
+	),
+	'columns' => array(
+		'sys_language_uid' => array(
+			'exclude' => 1,
+			'label' => 'LLL:EXT:lang/locallang_general.xml:LGL.language',
+			'config' => array(
+				'type' => 'select',
+				'foreign_table' => 'sys_language',
+				'foreign_table_where' => 'ORDER BY sys_language.title',
+				'items' => array(
+					array('LLL:EXT:lang/locallang_general.xlf:LGL.allLanguages', -1),
+					array('LLL:EXT:lang/locallang_general.xlf:LGL.default_value', 0)
+				),
+			),
+		),
+		'l10n_parent' => array(
+			'displayCond' => 'FIELD:sys_language_uid:>:0',
+			'exclude' => 1,
+			'label' => 'LLL:EXT:lang/locallang_general.xlf:LGL.l18n_parent',
+			'config' => array(
+				'type' => 'select',
+				'items' => array(
+					array('', 0),
+				),
+				'foreign_table' => 'tx_gomapsext_domain_model_map',
+				'foreign_table_where' => 'AND tx_gomapsext_domain_model_map.pid=###CURRENT_PID### AND tx_gomapsext_domain_model_map.sys_language_uid IN (-1,0)',
+			),
+		),
+		'l10n_diffsource' => array(
+			'config' => array(
+				'type' => 'passthrough',
+			),
+		),
+		't3ver_label' => array(
+			'label' => 'LLL:EXT:lang/locallang_general.xlf:LGL.versionLabel',
+			'config' => array(
+				'type' => 'input',
+				'size' => 30,
+				'max' => 255,
+			)
+		),
+		'hidden' => array(
+			'exclude' => 1,
+			'label' => 'LLL:EXT:lang/locallang_general.xlf:LGL.hidden',
+			'config' => array(
+				'type' => 'check',
+			),
+		),
+		'starttime' => array(
+			'exclude' => 1,
+			'l10n_mode' => 'mergeIfNotBlank',
+			'label' => 'LLL:EXT:lang/locallang_general.xlf:LGL.starttime',
+			'config' => array(
+				'type' => 'input',
+				'size' => 13,
+				'max' => 20,
+				'eval' => 'datetime',
+				'checkbox' => 0,
+				'default' => 0,
+				'range' => array(
+					'lower' => mktime(0, 0, 0, date('m'), date('d'), date('Y'))
+				),
+			),
+		),
+		'endtime' => array(
+			'exclude' => 1,
+			'l10n_mode' => 'mergeIfNotBlank',
+			'label' => 'LLL:EXT:lang/locallang_general.xlf:LGL.endtime',
+			'config' => array(
+				'type' => 'input',
+				'size' => 13,
+				'max' => 20,
+				'eval' => 'datetime',
+				'checkbox' => 0,
+				'default' => 0,
+				'range' => array(
+					'lower' => mktime(0, 0, 0, date('m'), date('d'), date('Y'))
+				),
+			),
+		),
+		'name' => array(
+			'exclude' => 0,
+			'label' => 'LLL:EXT:go_maps_ext/Resources/Private/Language/locallang_db.xlf:tx_gomapsext_domain_model_category.name',
+			'config' => array(
+				'type' => 'input',
+				'size' => 30,
+				'eval' => 'trim,nospace,alphanum,required'
+			),
+		),
+		'addresses' => array(
+			'exclude' => 1,
+			'label' => 'LLL:EXT:go_maps_ext/Resources/Private/Language/locallang_db.xlf:tx_gomapsext_domain_model_map.addresses',
+			'config' => array(
+				'type' => 'group',
+				'internal_type' => 'db',
+				'allowed' => 'tx_gomapsext_domain_model_address',
+				'foreign_table' => 'tx_gomapsext_domain_model_address',
+				'MM' => 'tx_gomapsext_map_address_mm',
+				'size' => 10,
+				'autoSizeMax' => 30,
+				'maxitems' => 9999,
+				'multiple' => 0,
+				'wizards' => array(
+					'_POSITION' => "right",
+					'_PADDING' => 4,
+					'_VERTICAL' => 0,
+					'_DISTANCE' => 2,
+					'edit' => array(
+						'type' => 'popup',
+						'title' => 'Edit',
+						'script' => 'wizard_edit.php',
+						'icon' => 'edit2.gif',
+						'popup_onlyOpenIfSelected' => 1,
+						'JSopenParams' => 'height=350,width=580,status=0,menubar=0,scrollbars=1',
+						),
+					'add' => Array(
+						'type' => 'script',
+						'title' => 'Create new',
+						'icon' => 'add.gif',
+						'params' => array(
+							'table' => 'tx_gomapsext_domain_model_address',
+							'pid' => '###CURRENT_PID###',
+							'setValue' => 'prepend'
+							),
+						'script' => 'wizard_add.php',
+					),
+				),
+			),
+		),
+	),
+);
+
+?>
