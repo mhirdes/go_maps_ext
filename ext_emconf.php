6d2d8c2895bd2ce6bfbbddbc2d0b219ebf042f02
Marc Hirdes <hirdes@clickstorm.de>
2013-12-02 09:31:00 +0100

    Version 1.4.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "go_maps_ext".
  *
- * Auto generated 27-11-2013 14:44
+ * Auto generated 02-12-2013 09:28
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array (
 	'description' => 'Google Maps Extension. Simply instert a google map Version 3, jQuery, Calculate Route, Image for Markers, Style Maps, KML, Categories...',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '1.4.0',
+	'version' => '1.4.1',
 	'dependencies' => 'extbase,fluid',
 	'conflicts' => '',
 	'priority' => '',
