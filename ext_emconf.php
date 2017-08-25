f49f85e02c2b53f3e138f1f85a9c1b9addf8c514
mhirdes <hirdes@clickstorm.de>
2017-08-25 16:42:42 +0200

    [TASK] Set version 2.3.1-dev

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = [
 	'description' => 'Google Maps Extension. Simply insert a google map Version 3 inc. jQuery, calculate a route,
 	                  images for markers, style maps, KML, categories, responsive and many more.',
 	'category' => 'plugin',
-	'version' => '2.3.0',
+	'version' => '2.3.1-dev',
 	'state' => 'stable',
 	'uploadfolder' => 1,
 	'modify_tables' => '',
