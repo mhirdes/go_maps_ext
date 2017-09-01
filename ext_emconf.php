7627d9046f302142e4c4a98ace4f1e138645b297
mhirdes <hirdes@clickstorm.de>
2017-09-01 16:38:31 +0200

    [BUGFIX] update emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,39 +1,23 @@
 <?php
 
-/***************************************************************
- * Extension Manager/Repository config file for ext "go_maps_ext".
- *
- * Auto generated 16-07-2014 15:35
- *
- * Manual updates:
- * Only the data in the array - everything else is removed by next
- * writing. "version" and "dependencies" must not be touched!
- ***************************************************************/
-
 $EM_CONF[$_EXTKEY] = [
-	'title' => 'Google Maps API Extbase',
-	'description' => 'Google Maps Extension. Simply insert a google map Version 3 inc. jQuery, calculate a route,
+    'title' => 'Google Maps API Extbase',
+    'description' => 'Google Maps Extension. Simply insert a google map Version 3 inc. jQuery, calculate a route,
 	                  images for markers, style maps, KML, categories, responsive and many more.',
-	'category' => 'plugin',
-	'version' => '2.3.1-dev',
-	'state' => 'stable',
-	'uploadfolder' => 1,
-	'modify_tables' => '',
-	'clearcacheonload' => 1,
-	'author' => 'Marc Hirdes',
-	'author_email' => 'marc_hirdes@gmx.de',
-	'author_company' => 'clickstorm GmbH',
-	'constraints' =>
-		[
-			'depends' =>
-				[
-					'typo3' => '7.6.0 - 8.9.99',
-				],
-			'conflicts' =>
-				[],
-			'suggests' =>
-				[],
-		],
-	'suggests' =>
-		[],
+    'category' => 'plugin',
+    'version' => '2.3.1-dev',
+    'state' => 'stable',
+    'uploadfolder' => 1,
+    'modify_tables' => '',
+    'clearcacheonload' => 1,
+    'author' => 'Marc Hirdes',
+    'author_email' => 'marc_hirdes@gmx.de',
+    'author_company' => 'clickstorm GmbH',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '7.6.0 - 8.7.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ]
 ];
