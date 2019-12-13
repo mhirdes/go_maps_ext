7c7dc1d98beac801a08a585150fdcc148c227482
mhirdes <hirdes@clickstorm.de>
2019-12-13 15:22:32 +0100

    [RELEASE] 3.2.0 - small features and bug fixes

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,7 +5,7 @@ $EM_CONF[$_EXTKEY] = [
     'description' => 'Google Maps Extension. Simply insert a google map Version 3 inc. jQuery, calculate a route,
 	                  images for markers, style maps, KML, categories, responsive and many more.',
     'category' => 'plugin',
-    'version' => '3.1.1-dev',
+    'version' => '3.2.0',
     'state' => 'stable',
     'uploadfolder' => 1,
     'clearCacheOnLoad' => 1,
