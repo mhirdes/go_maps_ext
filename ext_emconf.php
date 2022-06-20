81c998de6d52c6a4f546dce5a4046d4d2fc2fbbe
mhirdes <hirdes@clickstorm.de>
2022-06-20 11:47:51 +0200

    [TASK] set version to 5.1.1-dev

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,7 +5,7 @@ $EM_CONF[$_EXTKEY] = [
     'description' => 'Google Maps Extension. Simply insert a google map Version 3 inc. jQuery, calculate a route,
 	                  images for markers, style maps, KML, categories, responsive and many more.',
     'category' => 'plugin',
-    'version' => '5.1.0',
+    'version' => '5.1.1-dev',
     'state' => 'stable',
     'clearCacheOnLoad' => true,
     'author' => 'Marc Hirdes',
