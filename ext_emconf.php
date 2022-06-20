0cd11cdb39bdb8cf57e43cafb3ebfd4996b53489
mhirdes <hirdes@clickstorm.de>
2022-06-20 11:46:26 +0200

    [RELEASE] 5.1.0 - category in slugs, disable scroll zoom fix

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,7 +5,7 @@ $EM_CONF[$_EXTKEY] = [
     'description' => 'Google Maps Extension. Simply insert a google map Version 3 inc. jQuery, calculate a route,
 	                  images for markers, style maps, KML, categories, responsive and many more.',
     'category' => 'plugin',
-    'version' => '5.0.2-dev',
+    'version' => '5.1.0',
     'state' => 'stable',
     'clearCacheOnLoad' => true,
     'author' => 'Marc Hirdes',
