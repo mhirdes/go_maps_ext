0b8d48803d7adc689d2cb18bb89a5318e60ae9a9
mhirdes <hirdes@clickstorm.de>
2024-10-09 09:44:39 +0200

    [BUGFIX] frontend works again

--- Classes/ViewHelpers/EscapeViewHelper.php
+++ Classes/ViewHelpers/EscapeViewHelper.php
@@ -2,9 +2,7 @@
 
 namespace Clickstorm\GoMapsExt\ViewHelpers;
 
-use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
 use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
-use TYPO3Fluid\Fluid\Core\ViewHelper\Traits\CompileWithRenderStatic;
 
 /**
  * Renders a HTML-script value by passing it and escape ' to pretend for errors.
@@ -27,11 +25,9 @@ use TYPO3Fluid\Fluid\Core\ViewHelper\Traits\CompileWithRenderStatic;
  */
 class EscapeViewHelper extends AbstractViewHelper
 {
-    use CompileWithRenderStatic;
-
-    public static function renderStatic(array $arguments, \Closure $renderChildrenClosure, RenderingContextInterface $renderingContext): string
+    public function render(): string
     {
-        $value = $renderChildrenClosure() ?? '';
+        $value = $this->renderChildren() ?? '';
 
         return str_replace("'", "\'", preg_replace("/\r\n|\r|\n/", '', $value));
     }
