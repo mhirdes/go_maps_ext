a267d5e6dd188c7dc931b9656ce260440c5a81ff
Alexander König <koenig@clickstorm.de>
2025-11-25 14:37:34 +0100

    [TASK] set version to 7.1.3-dev

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,7 +5,7 @@ $EM_CONF[$_EXTKEY] = [
     'description' => 'Google Maps Extension. Simply insert a Google Map Version 3 without jQuery, calculate a route,
 	                  images for markers, style maps, KML, categories, responsive and many more.',
     'category' => 'plugin',
-    'version' => '7.1.2',
+    'version' => '7.1.3-dev',
     'state' => 'stable',
     'clearCacheOnLoad' => true,
     'author' => 'Marc Hirdes',
